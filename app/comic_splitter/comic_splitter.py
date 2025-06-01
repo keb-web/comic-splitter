03a8d681ed201d355b08f88033aed02e61567ab3
Kevin Le <kebinhleh@gmail.com>
2025-06-01 17:10:26 -0500

    Add basic rectangle detection using coputer vision library
    Basic working functionality. Untested for nested panels, complex
    layouts, and actual comic panels.

--- app/comic_splitter/comic_splitter.py
+++ app/comic_splitter/comic_splitter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 white_pixel = np.array([255, 255, 255])
 
@@ -6,13 +7,32 @@ class ComicSplitter:
     def __init__(self):
         pass
 
-    def split_page(self, page):
+    def split_page(self):
         return []
 
-    def determine_panel_bounds(self, page: np.ndarray):
-        height, width, channels = page.shape
+    def get_panel_contours(self, page: np.ndarray) -> list: 
+        edge_page = cv2.Canny(page, 30, 200)
+        contours, _ = cv2.findContours(
+            edge_page, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        approx_contours = []
+        for contour in contours:
+            epsilon = 0.1 * cv2.arcLength(contour, True)
+            approx_poly = cv2.approxPolyDP(contour, epsilon, False)
+            approx_contours.append(approx_poly)
+
+        return approx_contours
+
+    def draw_contour(self, page: np.ndarray, contour):
+        height, width = page.shape
+        blank_image = np.ones((height, width, 3), np.uint8)
+        cv2.drawContours(blank_image, contour, -1, (0, 255, 0), 3)
+        return blank_image
+
+    def get_panel_bounds(self, page: np.ndarray):
         black_pixels = np.all(page == [0, 0, 0], axis=-1)
         y_coords, x_coords = np.where(black_pixels)
         min_x, max_x = np.min(x_coords), np.max(x_coords)
         min_y, max_y = np.min(y_coords), np.max(y_coords)
         return [min_x, min_y, max_x, max_y]
+
--- tests/test_comic_splitter.py
+++ tests/test_comic_splitter.py
@@ -1,10 +1,11 @@
-# from PIL import Image
+import os
+from PIL import Image
+import sys
 import cv2
 import pytest
 import numpy as np
 
 from app.comic_splitter.comic_splitter import ComicSplitter
-
 
 # '/home/keb/Development/comic-splitter/samples/test_page_empty.jpg'
 
@@ -15,31 +16,55 @@ class TestComicSplitter:
         page_path = './tests/samples/test_page_empty.jpg'
         page_img = cv2.imread(page_path)
         splitter = ComicSplitter()
-        panels = splitter.split_page(page_img)
+        panels = splitter.split_page()
         assert panels == []
 
+    def test_get_multiple_panel_contours(self):
+        page_path = './tests/samples/test_page_stack_two_panel.jpg'
+        page_img = cv2.imread(page_path, cv2.IMREAD_GRAYSCALE)
+        splitter = ComicSplitter()
 
+        contours = splitter.get_panel_contours(page_img) 
+
+        top_panel_vertices = contours[0].shape[0]
+        bottom_panel_vertices = contours[1].shape[0]
+
+        assert len(contours) == 2
+        assert top_panel_vertices == 4
+        assert bottom_panel_vertices == 4
+
+    @pytest.mark.skip(reason='testing another imaging approach')
+    def test_determine_box_bounds_with_multiple_existing_panel(self):
+        page_path = './tests/samples/test_page_one_panel.jpg'
+        splitter = ComicSplitter()
+        # splitter.get_panel_bounds(page_)
+
+    @pytest.mark.skip(reason='testing another imaging approach')
     def test_determine_box_bounds_with_one_existing_panel(self):
         page_path = './tests/samples/test_page_one_panel.jpg'
         left, top, right, bottom = 311, 342, 1889, 2705
         page_img = cv2.imread(page_path)
         splitter = ComicSplitter()
-        assert splitter.determine_panel_bounds(page_img) == [
+        assert splitter.get_panel_bounds(page_img) == [
             left, top, right, bottom]
+
 
     @pytest.mark.skip(reason='need determine_box() func first')
     def test_split_page_with_basic_panel(self):
         page_path = './tests/samples/test_page_one_panel.jpg'
         page_img = cv2.imread(page_path)
         splitter = ComicSplitter()
-        # margin_value = 20
-        panels = splitter.split_page(page_img)
-        print()
-        self.view_image(page_img)
+        panels = splitter.split_page()
         assert panels == [page_img]
+
+    # TODO: refactor utility functions
 
     def view_image(self, img: np.ndarray):
         cv2.imshow('test_window', img)
         cv2.waitKey(0)
-
-# Bottom-right: (1889, 2705)
+    
+    def save_image(self, img: np.ndarray, filename: str = 'debug_output'):
+        project_root = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), '..'))
+        save_path = os.path.join(project_root, f'{filename}.jpg')
+        cv2.imwrite(save_path, img)
