1e951acd2ac3f16732e4016e02320033fa27e1be
Kevin Le <kebinhleh@gmail.com>
2025-06-03 17:50:50 -0500

    Add image preprocessing and fix epsilon for contour detection
    Fixes issue for detecting contours for panels with noisy edges. Panels
    previous contoured to be triangles are now rectangles.

--- app/comic_splitter/comic_splitter.py
+++ app/comic_splitter/comic_splitter.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import os
 import numpy as np
 import cv2
 
@@ -10,24 +12,61 @@ class ComicSplitter:
     def split_page(self):
         return []
 
+    def preprocessing(self, page: np.ndarray) -> tuple:
+        blur_page = cv2.GaussianBlur(page, (5, 5), 0)
+        # thresh_page = cv2.threshold(blur_page, 0, 255,
+        #                             cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        thresh_page = cv2.adaptiveThreshold(blur_page, 255, 
+                                cv2.ADAPTIVE_THRESH_MEAN_C, 
+                                cv2.THRESH_BINARY, 
+                                blockSize=11, C=2)
+        edge_page = cv2.Canny(thresh_page, 30, 200)
+
+        return blur_page, thresh_page, edge_page
+
+
     def get_panel_contours(self, page: np.ndarray) -> list: 
-        edge_page = cv2.Canny(page, 30, 200)
+        _, _, edge_page = self.preprocessing(page)
+
         contours, _ = cv2.findContours(
             edge_page, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+
         approx_contours = []
         for contour in contours:
-            epsilon = 0.1 * cv2.arcLength(contour, True)
+            epsilon = 0.02 * cv2.arcLength(contour, True)
             approx_poly = cv2.approxPolyDP(contour, epsilon, False)
             approx_contours.append(approx_poly)
 
         return approx_contours
 
-    def draw_contour(self, page: np.ndarray, contour):
+    # TODO: Encapsulate drawing to it's own class
+    def draw_contour(self, page: np.ndarray, contours: list, labels: bool):
         height, width = page.shape
         blank_image = np.ones((height, width, 3), np.uint8)
-        cv2.drawContours(blank_image, contour, -1, (0, 255, 0), 3)
+        cv2.drawContours(blank_image, contours, -1, (0, 255, 0), 3)
+        if labels:
+            self.label_contours(blank_image, contours)
         return blank_image
+
+    def label_contours(self, page, contours: list):
+        labels = defaultdict(list)
+
+        panel_number = len(contours)
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            center_x = x + w // 2
+            center_y = y + h // 2
+
+            cv2.putText(
+                page, str(panel_number),
+                (center_x, center_y),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1, (0,0,255), 2)
+            
+            labels[panel_number] = contour
+            panel_number-=1
+        return labels
 
     def get_panel_bounds(self, page: np.ndarray):
         black_pixels = np.all(page == [0, 0, 0], axis=-1)
@@ -35,4 +74,3 @@ class ComicSplitter:
         min_x, max_x = np.min(x_coords), np.max(x_coords)
         min_y, max_y = np.min(y_coords), np.max(y_coords)
         return [min_x, min_y, max_x, max_y]
-
--- tests/test_comic_splitter.py
+++ tests/test_comic_splitter.py
@@ -1,17 +1,98 @@
 import os
-from PIL import Image
-import sys
 import cv2
 import pytest
 import numpy as np
 
 from app.comic_splitter.comic_splitter import ComicSplitter
 
-# '/home/keb/Development/comic-splitter/samples/test_page_empty.jpg'
-
-
 class TestComicSplitter:
 
+    def test_get_stacked_panel_contours(self):
+        page_path = './tests/samples/test_page_stack_two_panel.jpg'
+        page_img = cv2.imread(page_path, cv2.IMREAD_GRAYSCALE)
+        splitter = ComicSplitter()
+
+        contours = splitter.get_panel_contours(page_img) 
+        print(contours)
+
+        top_panel_vertices = contours[0].shape[0]
+        bottom_panel_vertices = contours[1].shape[0]
+
+        assert len(contours) == 2
+        print(contours)
+        # assert contours.is_square()
+        assert top_panel_vertices == 4
+        assert bottom_panel_vertices == 4
+
+    def test_labeling_two_stacked_panels(self):
+        page_path = './tests/samples/test_page_stack_two_panel.jpg'
+        page_img = cv2.imread(page_path, cv2.IMREAD_GRAYSCALE)
+
+        splitter = ComicSplitter()
+        contours = splitter.get_panel_contours(page_img) 
+        label_dict = splitter.label_contours(page_img, contours)
+
+        # labeling panels from top to bottom
+        # TODO: CHECK each contour has 4 vertices
+        assert label_dict == {2: contours[0], 1: contours[1]}
+        assert len(contours) == 2
+
+    def test_labeling_multiple_side_by_side_panels(self):
+        page_path = './tests/samples/test_page_side_by_side.jpg'
+        page_img = cv2.imread(page_path, cv2.IMREAD_GRAYSCALE)
+        splitter = ComicSplitter()
+
+        contours = splitter.get_panel_contours(page_img) 
+        assert self.contour_is_rectangle(contours)
+        # label_dict = splitter.label_contours(page_img, contours)
+        # assert label_dict == {1: contours[0], 2: contours[1], 3: contours[2]}
+        # assert len(contours) == 3 
+
+    def contour_is_rectangle(self, contours) -> bool:
+        for contour in contours:
+            if contour.shape[0] != 4:
+                return False
+        return True
+
+    @pytest.mark.skip(reason='implementing right to left first')
+    def test_labeling_multiple_panels(self):
+        page_path = './tests/samples/test_page_multiple_panels.jpg'
+        page_img = cv2.imread(page_path, cv2.IMREAD_GRAYSCALE)
+
+        splitter = ComicSplitter()
+        contours = splitter.get_panel_contours(page_img) 
+        label_dict = splitter.label_contours(page_img, contours)
+
+        # self.draw_labels(page_img, contours, splitter)
+
+        # labeling panels from top to bottom & right to left
+        assert len(contours) == 6
+        assert label_dict == {2: contours[0], 1: contours[1]}
+
+    # TODO: refactor utility functions
+    # NOTE: -- Utility functions --
+
+    def draw_labels(self, img, contours, splitter):
+        height, width = img.shape
+        empty_image = np.zeros((height, width), dtype=np.int8)
+        labeled_image = splitter.draw_contour(empty_image, contours, True)
+        self.save_image(labeled_image)
+
+    def view_image(self, img: np.ndarray):
+        cv2.imshow('test_window', img)
+        cv2.waitKey(0)
+    
+    def save_image(self, img: np.ndarray,
+                   filename: str = 'debug_output'):
+        project_root = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), '..'))
+        save_path = os.path.join(project_root, f'{filename}.jpg')
+        cv2.imwrite(save_path, img)
+
+
+    # FIX: -- test below is doo doo dookie!! --
+
+    @pytest.mark.skip('pending other test completions')
     def test_split_page_with_no_panels(self):
         page_path = './tests/samples/test_page_empty.jpg'
         page_img = cv2.imread(page_path)
@@ -19,19 +100,25 @@ class TestComicSplitter:
         panels = splitter.split_page()
         assert panels == []
 
+
+    @pytest.mark.skip(reason='adding labeling images first')
     def test_get_multiple_panel_contours(self):
-        page_path = './tests/samples/test_page_stack_two_panel.jpg'
+        page_path = './tests/samples/test_page_multiple_panels.jpg'
         page_img = cv2.imread(page_path, cv2.IMREAD_GRAYSCALE)
         splitter = ComicSplitter()
-
         contours = splitter.get_panel_contours(page_img) 
 
-        top_panel_vertices = contours[0].shape[0]
-        bottom_panel_vertices = contours[1].shape[0]
+        # TODO: add labeling to ensure vertices are correct
+        # top_panel_vertices = contours[0].shape[0]
+        # bottom_panel_vertices = contours[1].shape[0]
+        print(contours)
 
-        assert len(contours) == 2
-        assert top_panel_vertices == 4
-        assert bottom_panel_vertices == 4
+        assert False
+        assert len(contours) == 6
+        # assert top_panel_vertices == 4
+        # assert bottom_panel_vertices == 4
+
+
 
     @pytest.mark.skip(reason='testing another imaging approach')
     def test_determine_box_bounds_with_multiple_existing_panel(self):
@@ -56,15 +143,3 @@ class TestComicSplitter:
         splitter = ComicSplitter()
         panels = splitter.split_page()
         assert panels == [page_img]
-
-    # TODO: refactor utility functions
-
-    def view_image(self, img: np.ndarray):
-        cv2.imshow('test_window', img)
-        cv2.waitKey(0)
-    
-    def save_image(self, img: np.ndarray, filename: str = 'debug_output'):
-        project_root = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '..'))
-        save_path = os.path.join(project_root, f'{filename}.jpg')
-        cv2.imwrite(save_path, img)
