3caf9409684270f346695f7e0bbbfaf5d2f95ffa
Kevin Le <kebinhleh@gmail.com>
2025-06-13 03:56:28 -0500

    Add basic cropping implementation

---
+++ app/comic_splitter/cropper.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+class ImageCropper:
+
+    def crop(self, image: np.ndarray,
+             crop_queue: list[tuple]) -> list:
+        cropped_images = []
+        for crop_values in crop_queue:
+            x, y, width, height = crop_values
+            cropped_image = image[y:y+height, x:x+width]
+            cropped_images.append(cropped_image)
+        return cropped_images
---
+++ tests/test_cropper.py
@@ -0,0 +1,22 @@
+import numpy as np
+from app.comic_splitter.cropper import ImageCropper
+from tests.page_utils import PageUtils
+
+
+class TestCropper:
+    
+    def test_crop_image_with_no_panels_returns_original_image(self):
+        indexed_panels = []
+        cropper = ImageCropper()
+        assert cropper.crop(np.ndarray((0,0)), indexed_panels) == []
+
+    def test_crop_image_with_one_panel_returns_correct_image_cropped(self):
+        util = PageUtils()
+        dummy_page = util.generate_page(
+            rectangle_coords=[((50, 50), (250, 250))],
+            page_height= 300, page_width = 300)
+        dummy_indexed_panels = [(50, 50, 200, 200)]
+        cropper = ImageCropper()
+        new_pages = cropper.crop(dummy_page, dummy_indexed_panels)
+        assert len(new_pages) == 1
+        assert new_pages[0].shape == (200, 200)
