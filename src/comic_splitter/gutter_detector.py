0853dfd67a3cad93264f3871395cce4b75e042ab
Kevin Le <kebinhleh@gmail.com>
2025-07-14 12:25:47 -0500

    Initiate GutterDetector
    attempt to fix panel-gap edge case

---
+++ src/comic_splitter/gutter_detector.py
@@ -0,0 +1,8 @@
+
+class GutterDetector:
+
+    def __init__(self):
+        pass
+
+    def detect_gutters(self):
+        pass
---
+++ tests/test_gutter_detector.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+from tests.page_utils import PageUtils
+
+
+class TestGutterDetector():
+
+    pass
+
