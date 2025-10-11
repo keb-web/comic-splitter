3c7a2c187a4734de2289a7a24e37ad99200ee298
Kevin Le <kebinhleh@gmail.com>
2025-10-10 20:47:41 -0500

    Add IN_MEMORY_DB flag

--- src/comic_splitter/db/database.py
+++ src/comic_splitter/db/database.py
@@ -1,7 +1,12 @@
+import os
 from comic_splitter.config import DB_FILE
 from sqlmodel import SQLModel, Session, create_engine
 
-sqlite_url = f'sqlite:///{DB_FILE}'
+
+USE_IN_MEMORY_DB = os.getenv("USE_IN_MEMORY_DB", "false").lower() == "true"
+
+sqlite_url = ("sqlite:///:memory:"
+              if USE_IN_MEMORY_DB else f"sqlite:///{DB_FILE}")
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 SQLModel.metadata.create_all(engine)
